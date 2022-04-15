03bdc0fc914c8e1d6b136fa742b3bccdd1f993de
YANG1511386 <81675148+YANG1511386@users.noreply.github.com>
2022-04-15 22:05:59 +0800

    pulltest

--- src/main/java/haha.java
+++ src/main/java/haha.java
@@ -3,5 +3,7 @@ haha {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello");
+        System.out.println("hello");
+        
     }
 }
